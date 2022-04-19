e72bd0a7882820eefeb4fa47f78514e6f22c1c32
Sxj1997 <1090306920@qq.com>
2022-04-19 17:09:08 +0800

    改为使用pytest-html

--- common.py
+++ common.py
@@ -3,6 +3,9 @@ import os
 # 当前路径
 PATH = os.path.dirname(__file__)
 
+# 测试路径
+test_dir = os.path.join(PATH, 'test_dir')
+
 # config文件路径
 config_file = os.path.join(PATH, 'config', 'config.yaml')
 
@@ -10,4 +13,13 @@ config_file = os.path.join(PATH, 'config', 'config.yaml')
 excel_file = os.path.join(PATH, 'data', 'data.xlsx')
 
 # log输出文件路径
-log_file = os.path.join(PATH, 'log\\')  # windows环境
+log_file = os.path.join(PATH, 'log\\')
+
+# html-report文件路径
+report_dir = os.path.join(PATH, 'pytest_report\\')
+
+# 失败次数
+max_fail = '3'
+
+# 重试次数
+rerun = '3'
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-# 实际项目选择使用pytest.ini或main.py
+
 import pytest
+import os
+from common import report_dir, max_fail, rerun
 
 
-def main():
-    pytest.main()
+# 实际项目选择使用pytest.ini或main.py
+def run():
+    html_report = os.path.join(report_dir, 'report.html')
+    xml_report = os.path.join(report_dir, 'report.xml')
+    pytest.main(["-s", "-v",
+                 "--html=" + html_report,
+                 "--junit-xml=" + xml_report,
+                 "--self-contained-html",
+                 "--maxfail", max_fail,
+                 "--reruns", rerun])
 
 
 if __name__ == '__main__':
-    main()
+    run()
